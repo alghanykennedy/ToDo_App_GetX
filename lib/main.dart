b49c681b17a25a300714a24232c45f80f3b17ac8
Alghany Kennedy <alghanyka@gmail.com>
2023-03-10 20:47:05 +0700

    part_2

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_todo_list/app/data/services/storage/services.dart';
+import 'package:getx_todo_list/app/modules/home/binding.dart';
+import 'package:getx_todo_list/app/modules/home/view.dart';
+import 'package:get/get.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo List using GetX',
+      home: const HomePage(),
+      initialBinding: HomeBinding(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
