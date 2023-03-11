13fcb39c6b68a57f1b4bc27196ddabbc1f523c88
Alghany Kennedy <alghanyka@gmail.com>
2023-03-11 09:26:52 +0700

    part_3

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:getx_todo_list/app/core/values/colors.dart';
 import 'package:getx_todo_list/app/data/models/task.dart';
 import 'package:getx_todo_list/app/modules/home/controller.dart';
 import 'package:getx_todo_list/app/core/utils/extensions.dart';
 import 'package:getx_todo_list/app/modules/home/widgets/add_card.dart';
+import 'package:getx_todo_list/app/modules/home/widgets/add_dialog.dart';
 import 'package:getx_todo_list/app/modules/home/widgets/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -32,7 +35,17 @@ class HomePage extends GetView<HomeController> {
                 physics: const ClampingScrollPhysics(),
                 children: [
                   ...controller.tasks
-                      .map((element) => TaskCard(task: element))
+                      .map((element) => LongPressDraggable(
+                          data: element,
+                          onDragStarted: () => controller.changeDeleting(true),
+                          onDraggableCanceled: (_, __) =>
+                              controller.changeDeleting(false),
+                          onDragEnd: (_) => controller.changeDeleting(false),
+                          feedback: Opacity(
+                            opacity: 0.8,
+                            child: TaskCard(task: element),
+                          ),
+                          child: TaskCard(task: element)))
                       .toList(),
                   // TaskCard(
                   //     task:
@@ -43,6 +56,22 @@ class HomePage extends GetView<HomeController> {
             )
           ],
         ),
+      ),
+      floatingActionButton: DragTarget<Task>(
+        builder: (_, __, ___) {
+          return Obx(
+            () => FloatingActionButton(
+              backgroundColor: controller.deleting.value ? Colors.red : blue,
+              onPressed: () =>
+                  Get.to(() => AddDialog(), transition: Transition.downToUp),
+              child: Icon(controller.deleting.value ? Icons.delete : Icons.add),
+            ),
+          );
+        },
+        onAccept: (Task task) {
+          controller.deleteTask(task);
+          EasyLoading.showSuccess('Delete Success');
+        },
       ),
     );
   }
